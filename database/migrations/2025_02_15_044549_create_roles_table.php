875bada8ddfde8039b3660cb76e93f9db27cebc3
Sithu Aung Za <sta@za.com.mm>
2025-02-15 11:35:46 +0630

    test

---
+++ database/migrations/2025_02_15_044549_create_roles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->jsonb('name');
+            $table->string('slug')->unique();
+            $table->jsonb('permissions')->default('[]');
+            $table->integer('level')->default(100); // 1 is highest level and max number is lowest label
+            $table->timestamps();
+
+            $table->index('slug');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('roles');
+    }
+};
